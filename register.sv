634c29c7d9acaab97ee611b3f8acfdbb26e28e23
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2023-10-22 13:18:18 -0700

    c

--- register.sv
+++ register.sv
@@ -17,11 +17,18 @@ module register # (parameter N = 8)
 //     1       1      0
 //     0       0     hold  (no change in output)
 //     0       1      in   (update output)
+   if(clear == 1 && load == 0) out <= 0 ;      
+    else (clear == 1 && load == 1) out <= 0 ;  
+    else (clear == 0 && load == 0) out <= out; 
+    else (clear == 0 && load == 1) out <= in; 
 
 
-	
+
 // Aside: What would be the impact of leaving posedge clear out of 
 //  the sensitivity list? 
+/*Ans: if leave ou the 'posedge clear' out, the 'clear' signal would not
+have an immediate effect on the 'out' signal. The register will clear 
+as soon as the 'clear' signal goes high regarless of the clk.*/
 end	
 		
 endmodule
